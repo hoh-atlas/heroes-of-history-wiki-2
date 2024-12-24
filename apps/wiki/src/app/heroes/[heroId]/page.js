8d990c50ef698f52916ce39c7f3e7d7c02a4c9bb
davidvach <vachalek.david@gmail.com>
2024-12-24 22:52:26 +0100

    feat: integrate Cloudflare Next.js support and add deployment scripts

--- apps/wiki/src/app/heroes/[heroId]/page.js
+++ apps/wiki/src/app/heroes/[heroId]/page.js
@@ -1,5 +1,7 @@
 import { allHeroes } from "../data";
 
+export const runtime = "edge";
+
 export async function generateStaticParams() {
     return allHeroes.map((hero) => ({
         heroId: hero.id,
