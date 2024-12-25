94ac3023ec9daea7bb327180dd5a97d37246f5da
davidvach <vachalek.david@gmail.com>
2024-12-25 20:24:39 +0100

    feat: enhance Cloudflare integration with development setup and update package configurations

--- apps/wiki/next.config.mjs
+++ apps/wiki/next.config.mjs
@@ -1,9 +1,14 @@
 import path from "path";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
